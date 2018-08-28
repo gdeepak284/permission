d241703dd85b32c52c3f3306a4da0314c3c5874c
gdeepak <gdeepak284@gmail.com>
2018-08-28 23:06:49 +0530

    added models

--- permission/permission/models.py
+++ permission/permission/models.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.db import models
 from django.contrib.postgres import fields as pg_fields
-from django.utils import timezone
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.utils.translation import ugettext as _
 from django.core.validators import RegexValidator
 
@@ -9,73 +10,140 @@ validate_mobile = RegexValidator(
 )
 
 
-class Patient(models.Model):
-    first_name = models.CharField(max_length=100, blank=True, default="")
-    last_name = models.CharField(max_length=100, blank=True, default="")
-    phone = models.CharField(max_length=20, validators=[validate_mobile],
-                             unique=True)
-    email = pg_fields.CIEmailField(blank=True, null=True)
-    problem = models.CharField(max_length=100, blank=True, default="Nothing")
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, phone, user_type, password=None):
+        if not email:
+            raise ValueError('Enter the email')
 
-    class Meta:
-        permissions = (
-            ("can_view_doctor", "Can view Doctor details"),
-            ("can_view_pharmacist", "Can edit Pharmacist details"),
+        if not name:
+            raise ValueError('Enter the Name')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            phone=phone,
+            user_type=user_type
         )
 
-    def __str__(self):
-        return self.get_full_name() or self.phone
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    @property
-    def full_name(self):
-        return self.get_full_name()
+    def create_superuser(self, email, name, phone, password):
+        user = self.create_user(email,
+                                password=password,
+                                name=name,
+                                phone=phone,
+                                user_type='NA'
+                                )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPES = (
+        ('PHT', 'Pharmacist'),
+        ('PAT', 'Patient'),
+        ('DOC', 'Doctor')
+    )
+    name = models.CharField(verbose_name='Full name', max_length=255)
+    email = models.EmailField(verbose_name='email address', max_length=50, unique=True)
+    created_at = models.DateTimeField(verbose_name='created at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='updated at', auto_now=True)
+    user_type = models.CharField(
+        verbose_name='user type',
+        max_length=2,
+        default='PA',
+        choices=USER_TYPES
+    )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    is_active = models.BooleanField(default=True,
+                                    help_text='active/inactive.')
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone']
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = 'User'
 
     def get_full_name(self):
-        full_name = "%s %s" % (self.first_name, self.last_name)
-        return full_name.strip()
+        return self.name
 
-    def get_short_name(self):
-        return self.first_name
-
-    def add_phone(self, phone_number, is_verified=None) -> "Phone":
-        return self.phones.create(phone=phone_number, is_verified=is_verified)
-
-    def add_email(self, email, is_verified=None) -> "Email":
-        return self.emails.create(email=email, is_verified=is_verified)
-
-
-class Pharmacist(models.Model):
-    name = models.CharField(max_length=100, blank=True, default="")
-    shop_name = models.CharField(max_length=100, blank=True, default="")
-    phone = models.CharField(max_length=20, validators=[validate_mobile],
-                             unique=True)
-    email = pg_fields.CIEmailField(blank=True, null=True)
-    address = models.CharField(max_length=100, blank=True, default="Nothing")
-
-    class Meta:
-        permissions = (
-            ("can_view_doctor", "Can view Doctor details"),
-            ("can_view_patient", "Can edit Patient details"),
-        )
+    def get_first_name(self):
+        return self.name.split(' ')[0]
 
     def __str__(self):
-        return self.name() or self.phone
-
-    @property
-    def name(self):
-        return self.name()
-
-    def get_shop_name(self):
-        return self.shop_name
-
-    def get_address(self):
-        return self.address
-
-    def get_email(self):
         return self.email
 
-    def add_phone(self, phone_number, is_verified=None) -> "Phone":
-        return self.phone.create(phone=phone_number, is_verified=is_verified)
 
-    def add_email(self, email, is_verified=None) -> "Email":
-        return self.email.create(email=email, is_verified=is_verified)
+class MedicalRecord(models.Model):
+    diagnosis = models.TextField(blank=True)
+    user = models.ForeignKey(to=User, related_name='medicalrecord_user')
+    doctor = models.ForeignKey(to=User, related_name='medicalrecord_doctor')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Doctor -> {}, User -> {}, Diagnosis -> {}".format(self.doctor.name,
+                                                            self.user.name,
+                                                            self.diagnosis)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('medicalrecord-detail', args=[str(self.id)])
+
+
+class Prescription(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(to=User, related_name='prescription_user')
+    doctor = models.ForeignKey(to=User, related_name='prescription_doctor')
+    medical_record = models.ForeignKey(to=MedicalRecord, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('prescription-detail', args=[str(self.id)])
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=100)
+    qty = models.IntegerField()
+    strength = models.IntegerField()
+    frequency = models.CharField(max_length=50)
+    prescription = models.ForeignKey(to=Prescription)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Approval(models.Model):
+    STATUS_TYPES = (
+        ('PE', 'Pending'),
+        ('RE', 'Rejected'),
+        ('AP', 'Approved')
+    )
+    prescription = models.ForeignKey(to=Prescription)
+    user = models.ForeignKey(to=User)
+    status = models.CharField(
+        max_length=2,
+        default='PE',
+        choices=STATUS_TYPES
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+
